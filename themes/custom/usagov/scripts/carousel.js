8225ae179f0c877841b6594a8d325f293a22c864
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2023-09-20 12:17:32 -0400

    Add A11Y Requests

--- themes/custom/usagov/scripts/carousel.js
+++ themes/custom/usagov/scripts/carousel.js
@@ -10,12 +10,12 @@ $(".slides").slick({
   "prevArrow":
     '<button class="previous slick-prev slick-arrow">' +
     '  <span class="sr-only">Previous slides</span>' +
-    '  <img src="Reimagined_Carousel_Left_Arrow.svg" alt="Go to previous card"/>' +
+    '  <img src="/themes/custom/usagov/images/Reimagined_Carousel_Left_Arrow.svg" alt="Go to previous card"/>' +
     "</button>",
   "nextArrow":
     '<button class="next slick-next slick-arrow">' +
     '  <span class="sr-only">Next slides</span>' +
-    '  <img src="Reimagined_Carousel_Right_Arrow.svg" alt="Go to next card"/>' +
+    '  <img src="/themes/custom/usagov/images/Reimagined_Carousel_Right_Arrow.svg" alt="Go to next card"/>' +
     "</button>",
   "responsive": [
     {
@@ -44,18 +44,64 @@ $(".slides").slick({
   ],
 });
 
-$(".slides").on("breakpoint", function (event, slick, breakpoint) {
-  removeTabAbility();
-});
+var carouselSlides = document.querySelector("#slides-list");
+var slideIndex;
 
-window.addEventListener("resize", removeTabAbility);
-window.addEventListener("load", removeTabAbility);
+addIndexAttributeToDots();
+setUpDotsListener();
+setUpNavButtonListeners();
+addAriaLabel();
 
-function removeTabAbility() {
-  var slideDots = document.querySelectorAll(
-    "#slidesList .slick-dots li button"
+function addIndexAttributeToDots() {
+  var dotsList = document.querySelectorAll(
+    "#slides-list .slick-dots li button"
   );
-  slideDots.forEach(function (buttonDot, index) {
-    buttonDot.setAttribute("tabindex", "-1");
+  dotsList.forEach((btn, i) => {
+    btn.setAttribute("dots-index", i);
   });
+}
+
+function setUpDotsListener() {
+  var dotsForListeners = document.querySelectorAll(
+    "#slides-list .slick-dots li button"
+  );
+  dotsForListeners.forEach((btn) => {
+    btn.addEventListener("click", moveFocusToCurrent);
+  });
+}
+
+function setUpNavButtonListeners() {
+  var navForListeners = document.querySelectorAll(".slick-arrow");
+  navForListeners.forEach((btn) => {
+    btn.addEventListener("click", updateAriaText);
+  });
+}
+
+function moveFocusToCurrent() {
+  window.setTimeout(function () {
+    var slideForFocus = document.querySelector(
+      "#slides-list .slick-list .slick-track .slick-slide.slick-current .slide a"
+    );
+    slideForFocus.focus({"focusVisible": true});
+  }, 200);
+}
+
+function addAriaLabel() {
+  var liveregion = document.createElement("div");
+  liveregion.setAttribute("aria-live", "polite");
+  liveregion.setAttribute("aria-atomic", "true");
+  liveregion.setAttribute("class", "liveregion visuallyhidden");
+  carouselSlides.appendChild(liveregion);
+}
+
+$(".slides").on(
+  "beforeChange",
+  function (event, slick, currentSlide, nextSlide) {
+    slideIndex = nextSlide + 1;
+  }
+);
+
+function updateAriaText() {
+  carouselSlides.querySelector(".liveregion").textContent =
+    "Slide " + slideIndex + " of 6";
 }
